2df7f13958be85975ece6193bdc9e6c63c8b7bdc
LOIC HAMERSCHMIDT <130869756+LO-HAMER@users.noreply.github.com>
2024-02-24 00:28:06 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const numbers = [5, 3, 9, 30];
+
+const smallestPowerOfTwo = arr => {
+      let results = [];
+      for (let i = 0; i < arr.length; i++) {
+            number = arr[i];
+
+        
+            j = 1;
+            while (j < number) {
+                  j = j * 2;
+            }
+            results.push(j);
+      }
+      return results
+}
+
+console.log(smallestPowerOfTwo(numbers)) 
